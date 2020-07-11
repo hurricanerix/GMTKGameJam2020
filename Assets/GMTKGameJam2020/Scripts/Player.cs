b484caf0ddcfcb57fe744d0704f3dc1086287a0e
Alan Erwin <aerwin3@gmail.com>
2020-07-11 16:25:03 -0500

    working towards thrust

--- Assets/GMTKGameJam2020/Scripts/Player.cs
+++ Assets/GMTKGameJam2020/Scripts/Player.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 [RequireComponent(typeof(Shooter))]
 public class Player : MonoBehaviour
 {
 
     [SerializeField]
-    public float rotationSpeed = 80.0f;
+    public float rotationSpeed = 80.0f; 
+    
+    [SerializeField]
+    public float thrustSpeed = 10.0f;
+
+
 
     Rigidbody rigidBody;
     private Shooter _shooter;
@@ -32,25 +38,50 @@ public class Player : MonoBehaviour
         {
             _shooter.Shoot(transform.position, transform.rotation.eulerAngles.y);
         }
+        Debug.DrawRay(transform.position, transform.rotation.eulerAngles);
+        HandleInput();
     }
 
-    private void handleInput()
+    private void HandleInput()
     {
         float ha = Input.GetAxis("Horizontal");
+        float thrust = Input.GetAxis("Thrust");
+        
+        if (ha != 0)
+        {
+            HandleRotation(ha);
+        }
+
+        if (thrust != 0)
+        {
+            HandleThrust();
+        }
+
+    }
+
+    private void HandleThrust()
+    {
+        print("Velocity : " + rigidBody.velocity);
+        rigidBody.velocity = new Vector3(rigidBody.velocity.x + thrustSpeed, 0, rigidBody.velocity.z + thrustSpeed);
+    }
+
+    private void HandleRotation(float inputRotation)
+    {
         Vector3 rotationalVelocity = new Vector3();
 
-        if (ha > 0)
+        if (inputRotation > 0)
         {
-
             rotationalVelocity.y = rotationSpeed;
-            //rotation.y = rotationSpeed * Time.deltaTime;
-        } else if (ha < 0)
+        }
+        else if (inputRotation < 0)
         {
             rotationalVelocity.y = -rotationSpeed;
         }
 
         Quaternion deltaRotation = Quaternion.Euler(rotationalVelocity * Time.deltaTime);
-        rigidBody.MoveRotation(rigidBody.rotation  * deltaRotation);
-
+        rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+        print("Rotation " + rigidBody.rotation);
     }
+
+
 }
