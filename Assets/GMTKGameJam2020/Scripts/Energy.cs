f2975d8881dc86e8b7b6a4300b54136c1683da56
Richard Hawkins <hurricanerix@gmail.com>
2020-07-11 13:08:08 -0500

    Add UI

---
+++ Assets/GMTKGameJam2020/Scripts/Energy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+    [SerializeField] private int _max = 100;
+    [SerializeField] private int _upkeep = 1;
+    [SerializeField] private Image _ui;
+    
+    [SerializeField]
+    [Range(0, 1)]
+    private float _uiT = 0.9f;
+
+    public int Current {get { return _current; }}
+
+    private int _current;
+
+    private void Start()
+    {
+        _current = _max;
+        _ui.fillAmount = 1;
+        StartCoroutine("Upkeep");
+    }
+
+    private IEnumerator Upkeep()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+            _current = Mathf.Max(0, _current - _upkeep);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        _ui.fillAmount = Mathf.Lerp(((float)_current) / _max, _ui.fillAmount, _uiT);        
+    }
+}
