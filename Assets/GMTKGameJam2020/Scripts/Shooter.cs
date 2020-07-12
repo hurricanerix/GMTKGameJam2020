590a82396833b760cddc4217670c42a706153184
Richard Hawkins <hurricanerix@gmail.com>
2020-07-12 09:46:30 -0500

    Add stuff

--- Assets/GMTKGameJam2020/Scripts/Shooter.cs
+++ Assets/GMTKGameJam2020/Scripts/Shooter.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] private GameObject _ammo;
+    [SerializeField] private int _cost = 1;
     public float rad = 0f;
 
     public void Shoot(Vector3 pos, float angle)
     {
+        var d = GetComponent<Energy>();
+        if (d != null)
+        {
+            d.Drain(_cost);
+        }
+
         var go = Instantiate(_ammo, pos, Quaternion.Euler(0f, angle, 0f));
     }
 }
