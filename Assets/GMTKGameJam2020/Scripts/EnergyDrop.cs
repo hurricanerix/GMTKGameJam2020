62fb56f4ef7931a1846e1ceb5c5d531094022dcc
Richard Hawkins <hurricanerix@gmail.com>
2020-07-12 12:17:50 -0500

    Add sfx for collecting energy

--- Assets/GMTKGameJam2020/Scripts/EnergyDrop.cs
+++ Assets/GMTKGameJam2020/Scripts/EnergyDrop.cs
@@ -1,14 +1,43 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class EnergyDrop : MonoBehaviour
 {
     [SerializeField] private int _amount;
+    [SerializeField] private AudioClip _collectClip;
+
+    private AudioSource _as;
+
+    private bool _collected = false;
+
+    private void Start()
+    {
+        _as = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (_collected)
+        {
+            return;
+        }
+
         Energy oe = other.GetComponent<Energy>();
         if (oe == null) return;
+        if (_as != null && _collectClip != null)
+        {
+            _as.PlayOneShot(_collectClip);
+        }
+        _collected = true;
+        transform.localScale = Vector3.zero;
         oe.Charge(_amount);
+        StartCoroutine("Destroy");
+    }
+
+    private IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(5);
         Destroy(gameObject);
     }
 
